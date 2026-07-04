f377e33eac2a2d2c611c6095681ed8a238ae92cc
Kajal <kajalkannaujiya94@gmail.com>
2026-07-04 12:44:16 +0530

    Enable init.sql

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -10,18 +10,18 @@ set -e
 #   sleep 3
 # done
 
-# echo "Running init.sql..."
-# php -r '
-#   $conn = new mysqli(getenv("DB_HOST"), getenv("DB_USER"), getenv("DB_PASS"), getenv("DB_NAME"));
-#   if ($conn->connect_error) { die("Connect failed: " . $conn->connect_error . "\n"); }
-#   $sql = file_get_contents("/var/www/html/init.sql");
-#   $sql = preg_replace("/CREATE DATABASE.*?;\n/i", "", $sql);
-#   $sql = preg_replace("/USE.*?;\n/i", "", $sql);
-#   foreach (array_filter(array_map("trim", explode(";", $sql))) as $stmt) {
-#     if (!$conn->query($stmt)) echo "WARN: " . $conn->error . "\n";
-#   }
-#   echo "Init complete.\n";
-# '
+ echo "Running init.sql..."
+ php -r '
+   $conn = new mysqli(getenv("DB_HOST"), getenv("DB_USER"), getenv("DB_PASS"), getenv("DB_NAME"));
+   if ($conn->connect_error) { die("Connect failed: " . $conn->connect_error . "\n"); }
+   $sql = file_get_contents("/var/www/html/init.sql");
+   $sql = preg_replace("/CREATE DATABASE.*?;\n/i", "", $sql);
+   $sql = preg_replace("/USE.*?;\n/i", "", $sql);
+   foreach (array_filter(array_map("trim", explode(";", $sql))) as $stmt) {
+     if (!$conn->query($stmt)) echo "WARN: " . $conn->error . "\n";
+   }
+   echo "Init complete.\n";
+ '
 
 echo "Starting Apache..."
 exec apache2 -D FOREGROUND
